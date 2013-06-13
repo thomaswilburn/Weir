f1413e960ad7da6f53a664de6fb9551d15d913d3
Thomas Wilburn <news@thomaswilburn.net>
2013-06-13 10:20:42 -0700

    Added concat task to create weir.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,21 @@ module.exports = function(grunt) {
           "public/css/rss.css": "public/css/rss.less"
         }
       } 
+    },
+    concat: {
+      dev: {
+        src: [
+          "public/js/Main.js",
+          "public/js/Controller.Stream.js",
+          "public/js/Controller.Settings.js",
+          "public/js/Service.Events.js",
+          "public/js/Service.LocalSettings.js",
+          "public/js/Service.Request.js",
+          "public/js/Service.Sanitize.js",
+          "public/js/Service.Server.js"
+        ],
+        dest: "public/js/weir.js"
+      }
     }
   }
   
--- public/js/Main.js
+++ public/js/Main.js
@@ -1,13 +1,17 @@
-var Weir = angular.module("Weir", []);
+(function() {
 
-//DIRECTIVES
-Weir.directive("preventDefault", function() {
-  return {
-    restrict: "A",
-    link: function(scope, element) {
-      element.bind("click", function(e) {
-        if (e.which == 1) e.preventDefault();
-      });
+  var Weir = angular.module("Weir", []);
+
+  //DIRECTIVES
+  Weir.directive("preventDefault", function() {
+    return {
+      restrict: "A",
+      link: function(scope, element) {
+        element.bind("click", function(e) {
+          if (e.which == 1) e.preventDefault();
+        });
+      }
     }
-  }
-});
+  });
+
+})();
