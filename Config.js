09a2d38585f4ea8c4502d9a00b7e49c52a9a5806
Thomas Wilburn <news@thomaswilburn.net>
2013-06-13 11:20:36 -0700

    Added error message on bad cfg.json

--- Config.js
+++ Config.js
@@ -1,5 +1,10 @@
 var fs = require("fs");
 var json = fs.readFileSync("cfg.json");
-var config = JSON.parse(json);
+try {
+  var config = JSON.parse(json);
+} catch (e) {
+  console.log("Couldn't parse cfg.json!", e);
+  process.exit();
+}
 
 module.exports = config;
