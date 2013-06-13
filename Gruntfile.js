e6741ab9c6868b4932656772cf0c88d8386a39fa
Thomas Wilburn <news@thomaswilburn.net>
2013-06-13 10:36:36 -0700

    Added grunt build, added to npm start

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,14 +31,10 @@ module.exports = function(grunt) {
     concat: {
       dev: {
         src: [
+          "public/js/angular.min.js",
           "public/js/Main.js",
-          "public/js/Controller.Stream.js",
-          "public/js/Controller.Settings.js",
-          "public/js/Service.Events.js",
-          "public/js/Service.LocalSettings.js",
-          "public/js/Service.Request.js",
-          "public/js/Service.Sanitize.js",
-          "public/js/Service.Server.js"
+          "public/js/Controller.*.js",
+          "public/js/Service.*.js",
         ],
         dest: "public/js/weir.js"
       }
@@ -46,5 +42,7 @@ module.exports = function(grunt) {
   }
   
   grunt.initConfig(options);
+  
+  grunt.registerTask("build", "Create client-side files", ["concat", "less"]);
   
 };
