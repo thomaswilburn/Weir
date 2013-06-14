1243787b1d128b932a984ba039c88c1952f226da
Thomas Wilburn <news@thomaswilburn.net>
2013-06-13 23:23:37 -0700

    Ensure that AJAX won't back up during TOTP authorization by halting/failing all new requests

--- public/js/Service.Request.js
+++ public/js/Service.Request.js
@@ -4,10 +4,13 @@
 
   //Weir.Request handles making requests with TOTP auth (eventually)
   Weir.service("Weir.Request", ["$http", "$q", function($http, $q) {
+    var halted = false;
+
     var authFactory = function(original, deferred) {
       //authorize loops on itself, until it gets a success
       var authorize = function(data) {
         if (data && data.success) {
+          halted = false;
           ajax(original).then(deferred.resolve);
           return;
         }
@@ -23,7 +26,8 @@
         request.error(authorize);
       };
 
-      setTimeout(authorize());
+      halted = true;
+      authorize();
     };
 
     var ajax = function(options) {
@@ -33,6 +37,10 @@
       };
       var params = angular.extend(defaults, options);
       var deferred = $q.defer();
+      if (halted) {
+        deferred.reject();
+        return deferred.promise;
+      }
       var request = $http(params);
       request.success(function(data) {
         //totp is not authorized?
