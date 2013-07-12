b9102f03cad36bca38f5009e8721a2d8ab6aa0d6
Thomas Wilburn <news@thomaswilburn.net>
2013-07-12 12:45:50 -0700

    Allow logging to be disabled by setting logPath to false

--- DevConsole.js
+++ DevConsole.js
@@ -1,14 +1,19 @@
 var Manos = require("./Manos");
 var fs = require("fs");
 var cfg = require("./Config");
-var logPath = cfg.logPath || "weir.log";
-var logStream = fs.createWriteStream(logPath, {encoding: "utf-8", flags: "a"});
+if (typeof cfg.logPath == "undefined" || cfg.logPath !== false) {
+  var logPath = cfg.logPath || "weir.log";
+  var logStream = fs.createWriteStream(logPath, {encoding: "utf-8", flags: "a"});
+}
 
 module.exports = {
   log: function() {
     var args = Array.prototype.slice.call(arguments);
     args.unshift(new Date().toGMTString() + " - ");
     console.log.apply(console, args);
+    if (cfg.logPath === false) {
+      return;
+    }
     var strings = args.map(function(item) { return JSON.stringify(item) }).join(" ") + "\n";
     logStream.write(strings);
   }
--- Hound.js
+++ Hound.js
@@ -155,7 +155,7 @@ var saveItems = function(feed, meta, articles) {
         added++;
       }
     });
-    //if (added) console.log("Added", added, "items from", meta.title);
+    if (added) console.log("Added", added, "items from", meta.title);
   });
 };
 
