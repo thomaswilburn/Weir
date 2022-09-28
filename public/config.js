17099dc981fce0248b4ad9d57d9d44467f280a9f
Thomas Wilburn <tbone@thomaswilburn.net>
2022-09-28 18:46:03 +0000

    remove weird svgs from Substack ads

--- public/config.js
+++ public/config.js
@@ -2,7 +2,8 @@ export var endpoint = "./";
 export var sanitizerBlocklist = [
   ".feedflare",
   ".mf-viral",
-  ".ebook-link-wrapper"
+  ".ebook-link-wrapper",
+  "figure a svg"
 ];
 // export var pingInterval = 10 * 1000;
 // export var updateLimit = 10;
